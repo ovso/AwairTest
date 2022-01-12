12b395ba4eede17c53814568e21e09eac91e6ee2
jaeho <ovsoce@gmail.com>
2022-01-13 02:49:33 +0900

    feat: 충돌(중복) 표시하기

--- app/src/main/java/io/github/ovso/schedule/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/ovso/schedule/ui/main/MainViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.ovso.schedule.data.EventUiModel
+import io.github.ovso.schedule.data.Events
 import io.github.ovso.schedule.data.ScheduleRepository
 import io.github.ovso.schedule.utils.TimeUtils
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -22,37 +24,41 @@ class MainViewModel @Inject constructor(
 
     private var nextPageToken: String? = null
 
-    fun fetchEvents() = viewModelScope.launch {
+    fun fetchEvents() = viewModelScope.launch(Dispatchers.IO) {
         try {
             if ((nextPageToken == "").not()) {
                 val data = repository.events(nextPageToken ?: "")
-                _items.value = _items.value?.toMutableList()?.apply {
-                    addAll(data.events.filter { it.title.isNullOrEmpty().not() }.map {
-                        EventUiModel(
-                            title = it.title,
-                            start = it.start,
-                            end = it.end,
-                            conflict = false
-                        )
-                    })
-                    sortedBy { TimeUtils.stringToSystemMillis(it.start!!) }
-                    val startList = map {
-                        TimeUtils.stringToSystemMillis(it.start!!)
-                    }
-                    forEachIndexed { i, uiModel1 ->
-                        val frequency = Collections.frequency(
-                            startList,
-                            TimeUtils.stringToSystemMillis(uiModel1.start!!)
-                        )
-                        if (frequency > 1) {
-                            set(i, uiModel1.copy(conflict = true))
-                        }
-                    }
-                }
+                _items.postValue(updateAndGet(data))
                 nextPageToken = data.nextPageToken
             }
         } catch (e: Exception) {
             e.printStackTrace()
+        }
+    }
+
+    private fun updateAndGet(data: Events): List<EventUiModel>? {
+        return _items.value?.toMutableList()?.apply {
+            addAll(data.events.filter { it.title.isNullOrEmpty().not() }.map {
+                EventUiModel(
+                    title = it.title,
+                    start = it.start,
+                    end = it.end,
+                    conflict = false
+                )
+            })
+            sortedBy { TimeUtils.stringToSystemMillis(it.start!!) }
+            val startList = map {
+                TimeUtils.stringToSystemMillis(it.start!!)
+            }
+            forEachIndexed { i, uiModel1 ->
+                val frequency = Collections.frequency(
+                    startList,
+                    TimeUtils.stringToSystemMillis(uiModel1.start!!)
+                )
+                if (frequency > 1) {
+                    set(i, uiModel1.copy(conflict = true))
+                }
+            }
         }
     }
 }
